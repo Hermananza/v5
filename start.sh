b90ea7f3800fd11036b3cd8bb9fd9096189a5974
Hermananza <101302614+Hermananza@users.noreply.github.com>
2025-04-24 01:18:22 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 cd /root
-curl -L -o R01FInject.deb "https://github.com/Hermananza/v5/R01FInject.deb"
+curl -L -o R01FInject.deb "https://github.com/Hermananza/v5/releases/download/v1.0.0/R01FInject.deb"
 
 dpkg -i R01FInject.deb
 rm /root/R01FInject.deb
